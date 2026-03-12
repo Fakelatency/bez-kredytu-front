0886831c9c7a82444d202ba721e9814cf58f38d5
Fakelatency <81415554+Fakelatency@users.noreply.github.com>
2026-03-12 19:25:12 +0100

    1

--- bez-kredytu/src/app/page.tsx
+++ bez-kredytu/src/app/page.tsx
@@ -1,10 +1,5 @@
 import ContactForm from "@/components/contactForm";
 import FAQSection from "@/components/faqSection";
-<<<<<<< HEAD
-=======
-// import FlexStatsSection from "@/components/flexComponent";
-// import FlexProveComponent from "@/components/flexProveComponent";
->>>>>>> parent of fbd6af1 (update)
 import Footer from "@/components/footer";
 import MainView from "@/components/mainView";
 import Navbar from "@/components/navbar";
